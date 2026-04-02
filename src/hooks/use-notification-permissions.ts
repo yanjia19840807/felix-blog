bf455401ef39cb699863ec0bcf9c877dc59cd134
yanjia <yanjiafelix@gmail.com>
2026-04-02 16:06:52 +0800

    fix bug

--- src/hooks/use-notification-permissions.ts
+++ src/hooks/use-notification-permissions.ts
@@ -1,13 +1,14 @@
 import Constants from 'expo-constants';
 import * as Notifications from 'expo-notifications';
 import { useCallback, useState } from 'react';
+import useToast from './use-toast';
 
 const appName = Constants?.expoConfig?.extra?.name || '';
 
 export const useNotificationPermissions = () => {
   const [notificationPermissions, setNoficationPermissions] =
     useState<Notifications.PermissionStatus>();
-
+  const toast = useToast();
   const requestNotificationPermissions = useCallback(async () => {
     const { status: existingStatus } = await Notifications.getPermissionsAsync();
     const finalStatus =
@@ -16,8 +17,12 @@ export const useNotificationPermissions = () => {
         : (await Notifications.requestPermissionsAsync()).status;
     setNoficationPermissions(finalStatus);
 
+    if (finalStatus !== 'granted') {
+      toast.info({ description: `请在 [系统设置] 里允许 ${appName} 发送通知。` });
+    }
+
     return finalStatus;
-  }, []);
+  }, [toast]);
 
   return {
     notificationPermissions,
